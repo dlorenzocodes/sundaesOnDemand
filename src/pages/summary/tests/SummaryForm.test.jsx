b02e559de5e67b5a12d2f7b1da1ac5466de8972b
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-09-16 14:56:33 -0500

    added test for popover element

--- src/pages/summary/tests/SummaryForm.test.jsx
+++ src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
@@ -26,5 +30,33 @@ describe("initial state of checkbox and button", () => {
 
     userEvent.click(checkbox);
     expect(orderButton).toBeDisabled();
+  });
+});
+
+describe("testing popover", () => {
+  test("popover should be hidden when page loads", () => {
+    render(<SummaryForm />);
+
+    const popOver = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popOver).not.toBeInTheDocument();
+  });
+
+  test("popover should be visible when hovered and hidden when not hovered", async () => {
+    render(<SummaryForm />);
+
+    const label = screen.getByText(/terms and conditions/i);
+    userEvent.hover(label);
+
+    const popOver = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popOver).toBeInTheDocument();
+
+    userEvent.unhover(label);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+    );
   });
 });
