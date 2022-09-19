430cd187d46f7abbd37f13c658f3cc72feaea22e
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-09-19 16:24:51 -0500

    wrote code to pass failing test and changed test to be async await

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Scoops from "./Scoops";
+import Row from "react-bootstrap/Row";
+
+function Options({ optionTypes }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3030/${optionTypes}`
+        );
+        const data = await response.data;
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getItems();
+  }, [optionTypes]);
+
+  const ItemComponent = optionTypes === "scoops" ? Scoops : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
+
+export default Options;
